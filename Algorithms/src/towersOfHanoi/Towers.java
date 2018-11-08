6c6b944720dda63226c506cf0bfdc030658ce212
martygelev <martygelev@gmail.com>
2018-11-08 08:56:44 +0200

    towers

---
+++ Algorithms/src/towersOfHanoi/Towers.java
@@ -0,0 +1,27 @@
+package towersOfHanoi;
+import java.util.Scanner;
+
+public class Towers {
+  public static int step = 1;
+    public static void main(String[] args) {
+        System.out.print("Enter number of discs: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOfDiscs = scanner.nextInt();
+
+        shift(numberOfDiscs, "Pole1", "Pole2", "Pole3");
+    }
+
+    public static void shift(int n, String startPole, String intermediatePole, String endPole)
+    {
+
+        if (n == 0)
+        {
+            return;
+        }
+        shift(n - 1, startPole, endPole, intermediatePole);
+        System.out.println("Step #"+ step++ + ": Move  disc \"" + n + "\" from " + startPole + " --> " + endPole);
+        shift(n - 1, intermediatePole, startPole, endPole);
+    }
+
+}
