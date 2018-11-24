7e56212d6686f58b6b2ec92083a8125cf92980cc
martygelev <martygelev@gmail.com>
2018-11-24 14:49:13 +0200

    exe

---
+++ demo/src/demo2/Main.java
@@ -0,0 +1,45 @@
+package demo2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        for (int i = 0; i<n ; i++)
+        {
+            int[] array;
+            array = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            boolean sym ;
+            sym=IsSymetric(array);
+
+            if (sym)
+            {
+                System.out.println("yes");
+            }
+            else System.out.println("no");
+        }
+
+    }
+
+    static boolean IsSymetric(int[] arr)
+    {
+        for(int i = 0; i< arr.length/2; i++)
+        {
+
+            if(arr[i] == arr[arr.length-1-i]){
+                continue;
+            }
+            else{
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
