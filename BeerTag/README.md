06ae8947613c8740695bbaa672acd4bfb7896e3d
martygelev <martygelev@gmail.com>
2019-02-22 11:44:25 +0200

    First commit for test

---
+++ BeerTag/README.md
@@ -0,0 +1,5 @@
+# BeerTag
+
+Beer Tag team project assignment (Team 6 -> Martin Gelev, Rosen Halachev )
+
+[Trello link](https://trello.com/b/Qe8MS0ax/beer-tag-team6)
---
+++ BeerTag/src/main/java/com/team6/beertag/BeertagApplication.java
@@ -0,0 +1,13 @@
+package com.team6.beertag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeertagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeertagApplication.class, args);
+    }
+
+}
